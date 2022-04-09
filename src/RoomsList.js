376f27e3509e56310f7581080a7390fcf9f768e3
gabriele.tasca <gabriele.tasca@gmail.com>
2022-04-09 19:14:39 +0200

    room list buttons

--- src/RoomsList.js
+++ src/RoomsList.js
@@ -10,8 +10,7 @@ export class RoomsList extends React.Component {
                 open rooms:
             </div>
 
-
-            {arrayItems(["poggers", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel", "mongrel"])}
+            {arrayItems(["poggers", "mongrel1", "mongrel2", "mongrel3", "mongrel4", "mongrel5", ])}
 
             </div>
 
@@ -23,23 +22,51 @@ export class RoomsList extends React.Component {
 
 function arrayItems(array) {
     return array.map((name) =>
-    <div key={name} style={{ display: 'flex', justifyContent: 'center'}}> 
-        {name}
-        <button> JOIN</button>
-    </div>
+        <RoomPanel key={name} name={name}/>
   );
 }
 
 
 export class RoomPanel extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {hover: false}
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseLeave = this.handleMouseLeave.bind(this);
+    }
+
     render() {
         return (
-            <div key={this.props.name} style={{ display: 'flex', justifyContent: 'center'}}> 
-            {this.props.name}
-            <button className='button3'> JOIN</button>
+        <div style={{ display: 'flex', justifyContent: 'space-between', padding: '5px'}} 
+        onMouseEnter={this.handleMouseEnter} onMouseLeave={this.handleMouseLeave} > 
+            {this.props.name} 
+            <div style={{width: '5%'}}/>
+            <HideableButton className='button3small' hidden={!this.state.hover} text='JOIN'/>  
         </div>
 
         );
     }
-  
+
+    handleMouseEnter() {
+        this.setState({hover: true})
+    }
+    handleMouseLeave() {
+        this.setState({hover: false})
+    }
+}
+
+
+export class HideableButton extends React.Component {
+
+    render() {
+        return (
+            <button 
+            className='button3small' 
+            style={this.props.hidden? {opacity: '0'}:undefined}  
+            > 
+                {this.props.text} 
+            </button>
+            );
+    }
+
 }
