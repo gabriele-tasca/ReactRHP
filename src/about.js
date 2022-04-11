9bf9077970ec7fa3467301c514aa728fdcc7a60b
gabriele.tasca <gabriele.tasca@gmail.com>
2022-04-11 12:13:29 +0200

    basic about page

--- src/about.js
+++ src/about.js
@@ -9,11 +9,16 @@ export class About extends React.Component {
 
     render() {
         return (
-        <div className='gridcontainer' >
-            <div>Mongrels</div>
-            <div>Mongrels</div>
-            <div>Mongrels</div>
-            <div>Mongrels</div>
+        <div style={{display:'flex', flexDirection: 'column', justifyContent:'center', alignContent:'center',}}>
+
+            <div className='gridentry'> A React UI for a multiplayer web game </div>
+            <div style={{height:'50px'}}></div>
+            <div style={{display:'flex', flexDirection: 'row', justifyContent:'center', alignContent:'center',}} >
+                <div className='gridentry'>Responsive UI adapts to the screen size</div>
+                <div className='gridentry'>DOM routing allows to join rooms via direct link </div>
+                <div className='gridentry'>The room list is updated in real time based on data provided by the WS server</div>
+
+            </div>
         </div>
         );
     }
