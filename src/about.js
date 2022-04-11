992a9b2eaa5cc0689b7c43dd8250bd675ee5b391
gabriele.tasca <gabriele.tasca@gmail.com>
2022-04-11 12:50:17 +0200

    icons and better flexing on about page

--- src/about.js
+++ src/about.js
@@ -1,24 +1,45 @@
 import React from 'react';
-
+import { ReactComponent as IconAutorenew } from './icons/autorenew_black_24dp.svg';
+import { ReactComponent as IconBolt } from './icons/bolt_black_24dp.svg';
+import { ReactComponent as IconRoute } from './icons/fork_left_black_24dp.svg';
 
 export class About extends React.Component {
-    constructor(props) {
-        super(props);
-        // this.state = {value: this.props.startingText}; // non-placeholder default text
-    }
 
     render() {
         return (
-        <div style={{display:'flex', flexDirection: 'column', justifyContent:'center', alignContent:'center',}}>
+        <div style={{display:'flex', flexDirection: 'column', justifyContent:'center', alignContent:'center', overflowY:'auto' }}>
+            <div style={{display:'flex', justifyContent:'center', alignContent:'center', overflowY:'auto' }} >
 
-            <div className='gridentry'> A React UI for a multiplayer web game </div>
+                <div className='gridentrytop'> A React UI for a multiplayer web game </div>
+            </div>
             <div style={{height:'50px'}}></div>
-            <div style={{display:'flex', flexDirection: 'row', justifyContent:'center', alignContent:'center',}} >
-                <div className='gridentry'>Responsive UI adapts to the screen size</div>
+            <div style={{display:'flex', alignItems:'flex-start', flexDirection: 'row', justifyContent:'center', alignContent:'center', flexWrap:'wrap', }} >
+
+                <GridEntry icon={<IconAutorenew/>} text={'Responsive UI adapts to the screen size'}/>
+                <GridEntry icon={<IconRoute/>} text={'DOM routing allows to join rooms via direct link'}/>
+                <GridEntry icon={<IconBolt/>} text={'The room list is synced to the server in real time'}/>
+
+                {/* <div className='gridentry'>Responsive UI adapts to the screen size</div>
+
                 <div className='gridentry'>DOM routing allows to join rooms via direct link </div>
-                <div className='gridentry'>The room list is updated in real time based on data provided by the WS server</div>
+
+                <div className='gridentry'>The room list is updated in real time based on data provided by the WS server</div> */}
+
 
             </div>
+        </div>
+        );
+    }
+}
+
+export class GridEntry extends React.Component {
+    render() {
+        return (
+
+            <div className='gridentry' style={{display:'flex', flexDirection: 'column', justifyContent:'center', alignContent:'center' }}>
+            <div > {this.props.icon} </div>
+            <div style={{height:'10px'}}></div>
+            <div > {this.props.text} </div>
         </div>
         );
     }
